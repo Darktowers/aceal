cf0fa2ec1d07a55c82506e2aeca090f3258a822b
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2016-12-16 21:51:21 -0500

    Pruebas server

--- conect.php
+++ conect.php
@@ -4,9 +4,9 @@
 	//$host="localhost";
 	//$pass="";
 	//$db="aceal";
-	$host=br-cdbr-azure-south-b.cloudapp.net;
-	$usuario=bf24d6a146c400;
-	$pass=4fe43f28;
+	$host="br-cdbr-azure-south-b.cloudapp.net";
+	$usuario="bf24d6a146c400";
+	$pass="4fe43f28";
 	$db="acceal";
   $mysql=new mysqli($host,$usuario,$pass,$db) or die("No se puede conectar");
 	
