7c9a1f2499da4a8fdc8b9f19b2e4bd75a0b583ef
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2016-12-16 21:43:12 -0500

    Pruebas server

--- conect.php
+++ conect.php
@@ -8,5 +8,6 @@
 	$usuario=bf24d6a146c400;
 	$pass=4fe43f28;
 	$db="acceal";
-  $mysql=new mysqli($host,$usuario,$pass,$db);
+  $mysql=new mysqli($host,$usuario,$pass,$db) or die("No se puede conectar");
+	
 ?>
